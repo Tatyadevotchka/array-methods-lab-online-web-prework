fb77a5927ae76d805c9a4b8b13326f5a768bebb9
Ana Riat <t.riat24@gmail.com>
2019-05-12 08:17:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,11 +4,12 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  sorted_array= array.sort
-
+  sorted_array= array.sorted_array
+  sorted_array
 end
 
 def using_reverse(array)
+  
 
 end
 
